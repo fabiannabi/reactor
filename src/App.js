e50bb936d934eea93c416f4a0ad03d9e224cd3f4
fabiannabi <fabiannabi@yahoo.com.mx>
2022-10-15 22:54:38 -0500

    add type input

--- src/App.js
+++ src/App.js
@@ -8,10 +8,19 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(e.target.xa.value);
-    console.log(e.target.xb.value);
-    console.log(e.target.xd.value);
+    setValues({
+      tipo: e.target.tipo.value,
+      xa: e.target.xa.value,
+      xb: e.target.xb.value,
+      xd: e.target.xd.value,
+    });
   };
+
+  const startRun = () => {
+    // do something
+    // exponential base 10 5*Math.pow(10, -1)
+  };
+  console.log(values);
 
   return (
     <div className="bg-gray-200 h-screen w-screen">
@@ -22,6 +31,37 @@ function App() {
           onSubmit={(e) => handleSubmit(e)}
           className="flex items-center justify-center gap-x-4 flex-col gap-y-8"
         >
+          {" "}
+          <h1> Tipo de ecuacion</h1>
+          <section className="flex items-center justify-center flex-col gap-2">
+            <section className="flex items-center justify-around gap-x-2">
+              <p>0:</p>
+              <p>1 reactivo 1 producto</p>
+            </section>
+
+            <section className="flex items-center justify-around gap-x-2">
+              <p>1:</p>
+              <p>1 reactivo 2 producto</p>
+            </section>
+            <section className="flex items-center justify-around gap-x-2">
+              <p>2:</p>
+              <p>2 reactivo 1 producto</p>
+            </section>
+            <section className="flex items-center justify-around gap-x-2">
+              <p>3:</p>
+              <p>2 reactivo 2 producto</p>
+            </section>
+
+            <input
+              className="border-2 border-black rounded-full text-center"
+              type="number"
+              name="tipo"
+              placeholder="tipo de ecuacion"
+              min={0}
+              max={3}
+            ></input>
+          </section>
+          <h1> valores iniciales</h1>
           <section className="flex items-center justify-between">
             <input
               className="border-2 border-black rounded-full text-center"
@@ -45,15 +85,22 @@ function App() {
               min={0}
             ></input>
           </section>
-
           <button
             type="submit"
             className="w-[300px] rounded-xl text-lg text-center bg-blue-400"
           >
             {" "}
-            Send Values
+            Set initial Values
           </button>
         </form>
+      </section>
+      <section className="flex items-center justify-center pt-6">
+        <button
+          onClick={startRun}
+          className="bg-green-500 w-[300px] rounded-xl text-lg text-center"
+        >
+          Start Run
+        </button>
       </section>
     </div>
   );
