8220c6a79013f188e108555c363882a62e777826
fabiannabi <fabiannabi@yahoo.com.mx>
2022-10-15 20:55:41 -0500

    updates tailwind for instalation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import logo from "./logo.svg";
 import "./App.css";
 
+import { useState, useEffect } from "react";
+
 function App() {
+  const [values, setValues] = useState({});
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target.xa.value);
+    console.log(e.target.xb.value);
+    console.log(e.target.xd.value);
+  };
+
   return (
     <div className="bg-gray-200 h-screen w-screen">
       <section className="flex items-center justify-center gap-y-6 flex-col">
-        <h1 class="text-3xl font-bold underline ">Reactor app</h1>
+        <h1 className="text-3xl font-bold underline ">Reactor app</h1>
 
-        <form className="flex items-center justify-center gap-x-4 flex-col gap-y-8">
+        <form
+          onSubmit={(e) => handleSubmit(e)}
+          className="flex items-center justify-center gap-x-4 flex-col gap-y-8"
+        >
           <section className="flex items-center justify-between">
             <input
               className="border-2 border-black rounded-full text-center"
@@ -20,7 +34,7 @@ function App() {
               className="border-2 border-black rounded-full text-center"
               type="number"
               name="xb"
-              placeholder="xc"
+              placeholder="xb"
               min={0}
             ></input>
             <input
