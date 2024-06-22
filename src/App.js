63d0b6cbb6d90c2a9ffa33e8143073e208ffe8e9
fabiannabi <fabiannabi@yahoo.com.mx>
2024-06-22 17:12:37 -0600

    Adds endpoint

--- src/App.js
+++ src/App.js
@@ -1,108 +1,75 @@
-import logo from "./logo.svg";
 import "./App.css";
+import axios from 'axios';
+import styled from "styled-components";
 
-import { useState, useEffect } from "react";
+const apiUrl = 'http://localhost:3001';
 
 function App() {
-  const [values, setValues] = useState({});
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setValues({
-      tipo: e.target.tipo.value,
-      xa: e.target.xa.value,
-      xb: e.target.xb.value,
-      xd: e.target.xd.value,
+  axios.get(`${apiUrl}/your_endpoint`)
+    .then(response => {
+      console.log(response.data);
+    })
+    .catch(error => {
+      console.error('There was an error!', error);
     });
-  };
 
-  const startRun = () => {
-    // do something
-    // exponential base 10 5*Math.pow(10, -1)
-  };
-  console.log(values);
+  const WaveContainer = styled.div`
+    background: linear-gradient(to bottom, #00b2d6, #00b2d6);
+    position: relative;
+    height: 100vh;
+  `;
+
+  const WaveAnimation = styled.div`
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 40vh;
+    background: linear-gradient(to bottom, #d65900, #d69e00);
+    background-size: cover;
+
+    /* Add animation keyframes to create a wave effect */
+    animation: wave 10s linear infinite;
+    @keyframes wave {
+      0% {
+        margin-left: 0;
+      }
+      25% {
+        margin-left: -50px;
+      }
+      50% {
+        margin-left: -100px;
+      }
+      75% {
+        margin-left: -150px;
+      }
+      100% {
+        margin-left: -200px;
+      }
+    }
+  `;
+  const Container = styled.div`
+    display: flex;
+    align-items: center;
+    height: 100%;
+  `;
+
+  const LeftSection = styled.div`
+    width: 50%;
+    padding: 0 2rem;
+    text-align: center;
+  `;
+
+  const RightSection = styled.div`
+    width: 50%;
+  `;
 
   return (
-    <div className="bg-gray-200 h-screen w-screen">
-      <section className="flex items-center justify-center gap-y-6 flex-col">
-        <h1 className="text-3xl font-bold underline ">Reactor app</h1>
+    <WaveContainer>
+      <WaveAnimation />
 
-        <form
-          onSubmit={(e) => handleSubmit(e)}
-          className="flex items-center justify-center gap-x-4 flex-col gap-y-8"
-        >
-          {" "}
-          <h1> Tipo de ecuacion</h1>
-          <section className="flex items-center justify-center flex-col gap-2">
-            <section className="flex items-center justify-around gap-x-2">
-              <p>0:</p>
-              <p>1 reactivo 1 producto</p>
-            </section>
-
-            <section className="flex items-center justify-around gap-x-2">
-              <p>1:</p>
-              <p>1 reactivo 2 producto</p>
-            </section>
-            <section className="flex items-center justify-around gap-x-2">
-              <p>2:</p>
-              <p>2 reactivo 1 producto</p>
-            </section>
-            <section className="flex items-center justify-around gap-x-2">
-              <p>3:</p>
-              <p>2 reactivo 2 producto</p>
-            </section>
-
-            <input
-              className="border-2 border-black rounded-full text-center"
-              type="number"
-              name="tipo"
-              placeholder="tipo de ecuacion"
-              min={0}
-              max={3}
-            ></input>
-          </section>
-          <h1> valores iniciales</h1>
-          <section className="flex items-center justify-between">
-            <input
-              className="border-2 border-black rounded-full text-center"
-              type="number"
-              name="xa"
-              placeholder="xa"
-              min={0}
-            ></input>
-            <input
-              className="border-2 border-black rounded-full text-center"
-              type="number"
-              name="xb"
-              placeholder="xb"
-              min={0}
-            ></input>
-            <input
-              className="border-2 border-black rounded-full text-center"
-              type="number"
-              name="xd"
-              placeholder="xd"
-              min={0}
-            ></input>
-          </section>
-          <button
-            type="submit"
-            className="w-[300px] rounded-xl text-lg text-center bg-blue-400"
-          >
-            {" "}
-            Set initial Values
-          </button>
-        </form>
-      </section>
-      <section className="flex items-center justify-center pt-6">
-        <button
-          onClick={startRun}
-          className="bg-green-500 w-[300px] rounded-xl text-lg text-center"
-        >
-          Start Run
-        </button>
-      </section>
-    </div>
+    </WaveContainer>
   );
 }
 
